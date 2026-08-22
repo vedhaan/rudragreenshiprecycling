0c859373f11eb0a9c3be8fe25c043eac641bbdb0
nihaljoshi1 <contact.nihal8@gmail.com>
2026-08-22 15:29:30 +0530

    About page complete, design system established

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
